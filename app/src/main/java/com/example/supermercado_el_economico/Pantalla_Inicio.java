3b69a3af3474db7d860e98012cc8a51b433c388f
ClaudethMatute <150222554+ClaudethMatute@users.noreply.github.com>
2024-02-19 17:59:58 -0600

    agregado de pantallas de login

---
+++ app/src/main/java/com/example/supermercado_el_economico/Pantalla_Inicio.java
@@ -0,0 +1,14 @@
+package com.example.supermercado_el_economico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla_Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicio);
+    }
+}
