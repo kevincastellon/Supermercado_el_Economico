8e3cbc747b6b1cc1ba6a4835f8e04c54568780f2
ClaudethMatute <150222554+ClaudethMatute@users.noreply.github.com>
2024-02-21 21:05:00 -0600

    modificacion de l apnatlla login

--- app/src/main/java/com/example/supermercado_el_economico/MainActivity.java
+++ app/src/main/java/com/example/supermercado_el_economico/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.supermercado_el_economico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Variables para los componentes de Material Design
+    private TextInputEditText txtcorreoEn;
+    private TextInputEditText txtpasswordEntrada;
+    private MaterialButton btnentrar,btnCrear,btnestablecer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtcorreoEn = findViewById(R.id.txtcorreoEn);
+        txtpasswordEntrada = findViewById(R.id.txtpasswordEn);
+        btnentrar = findViewById(R.id.btnentrar);
+        btnCrear = findViewById(R.id.btnCrear);
+        btnestablecer = findViewById(R.id.btnrestablecer);
+
+        btnestablecer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Restablecer_contra.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/supermercado_el_economico/Pantalla_verificacion.java
+++ app/src/main/java/com/example/supermercado_el_economico/Pantalla_verificacion.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Pantalla_Login extends AppCompatActivity {
+public class Pantalla_verificacion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pantalla_login);
+        setContentView(R.layout.activity_pantalla_verificacion);
     }
 }
