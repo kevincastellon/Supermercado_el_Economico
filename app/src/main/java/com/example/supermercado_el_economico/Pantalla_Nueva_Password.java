cf3a662a8c47964f10a2074dc32edf651998e3a5
ClaudethMatute <150222554+ClaudethMatute@users.noreply.github.com>
2024-02-22 00:44:00 -0600

    restablecer contraseña pantalla diseñada

--- app/src/main/java/com/example/supermercado_el_economico/Pantalla_Nueva_Password.java
+++ app/src/main/java/com/example/supermercado_el_economico/Pantalla_Nueva_Password.java
@@ -2,13 +2,33 @@ package com.example.supermercado_el_economico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Pantalla_Nueva_Password extends AppCompatActivity {
-
+    // Variables para los componentes de Material Design
+    private TextInputEditText txtpassNew;
+    private TextInputEditText txtpassVeriNew;
+    private MaterialButton btncanbiarcont;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_nueva_password);
+
+        txtpassNew =(TextInputEditText) findViewById(R.id.txtpassNew);
+        txtpassVeriNew =(TextInputEditText) findViewById(R.id.txtpassVeriNew);
+        btncanbiarcont =(MaterialButton) findViewById(R.id.btnCambiarContra);
+
+        btncanbiarcont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
