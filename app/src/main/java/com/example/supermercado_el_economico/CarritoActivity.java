337eb7684dcc85fa3f6cff814e27404a7f117ed1
kevincastellon <158013293+kevincastellon@users.noreply.github.com>
2024-03-09 21:09:19 -0600

    carrito
    carrito

--- app/src/main/java/com/example/supermercado_el_economico/CarritoActivity.java
+++ app/src/main/java/com/example/supermercado_el_economico/CarritoActivity.java
@@ -1,10 +1,19 @@
 package com.example.supermercado_el_economico;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CarritoActivity extends AppCompatActivity {
+
+    BottomNavigationView btninferior;
+    RecyclerView miadapter;
+    SearchView misearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/supermercado_el_economico/PedidosActivity.java
+++ app/src/main/java/com/example/supermercado_el_economico/PedidosActivity.java
@@ -1,15 +1,38 @@
 package com.example.supermercado_el_economico;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PedidosActivity extends AppCompatActivity {
 
+    Button btncarrito, btnhistorial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedidos);
+
+        btncarrito = (Button) findViewById(R.id.btncarrito);
+        btnhistorial = (Button) findViewById(R.id.btnhistorial);
+
+        btncarrito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CarritoActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnhistorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),HistorialPedidoActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
